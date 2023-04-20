614963e8754db6928fe24796086400bb9e17ef6c
Yahwon <37835539+Ya-hwon@users.noreply.github.com>
2023-04-20 21:15:00 +0200

    0.2.1 (#2)
    * +sort by cached key
    * +extend tests
    * #move tests&benches to respective dir
    * #use default trait instead of own method
    * #logic fixes
    * #cleanup
    * #borrow vec->slice

--- src/hprtree.rs
+++ src/hprtree.rs
@@ -11,19 +11,21 @@ pub struct BBox {
     pub maxy: f32,
 }
 
-impl BBox {
+impl Default for BBox {
     /// The default of the bbox is min = f32::MAX and max = f32::MIN
     ///
     /// This is so that "expanding to include"ing such a bbox results in whatever was used to expand the bbox by
-    pub fn default() -> BBox {
-        BBox {
+    fn default() -> Self {
+        Self {
             minx: f32::MAX,
             miny: f32::MAX,
             maxx: f32::MIN,
             maxy: f32::MIN,
         }
     }
+}
 
+impl BBox {
     pub fn new(minx: f32, miny: f32, maxx: f32, maxy: f32) -> BBox {
         BBox {
             minx,
@@ -181,10 +183,10 @@ fn hilbert_xy_to_index(x: u32, y: u32) -> u32 {
     let i0 = x ^ y;
     let i1 = b | (0xFFFF ^ (i0 | a));
 
-    return ((interleave(i1) << 1) | interleave(i0)) >> (32 - 2 * HILBERT_LEVEL);
+    ((interleave(i1) << 1) | interleave(i0)) >> (32 - 2 * HILBERT_LEVEL)
 }
 
-fn get_layer_size(layer: usize, layer_start_index: &Vec<usize>) -> usize {
+fn get_layer_size(layer: usize, layer_start_index: &[usize]) -> usize {
     layer_start_index[layer + 1] - layer_start_index[layer]
 }
 
@@ -226,6 +228,14 @@ impl<T> HPRTreeBuilder<T>
 where
     T: Clone,
 {
+    /// Creates a new tree builder with base capacity
+    pub fn new(size: usize) -> Self {
+        HPRTreeBuilder {
+            items: Vec::with_capacity(size),
+            extent: BBox::default(),
+        }
+    }
+
     /// Inserts an element into the index
     pub fn insert(&mut self, item: T, geom: Point) {
         self.extent.expand_to_include_point(&geom);
@@ -235,62 +245,7 @@ where
         });
     }
 
-    /// Sorts the contained data, if this has been called before [build](#method.build), prefer [build_sorted](#method.build_sorted) instead
-    pub fn sort_items(&mut self) {
-        let stride_x = self.extent.width() / H as f32;
-        let stride_y = self.extent.height() / H as f32;
-
-        let extent_min_x = self.extent.minx;
-
-        self.items.sort_unstable_by(|lhs, rhs| {
-            let xlhs: u32 = ((lhs.index_geom.x - extent_min_x) / stride_x).trunc() as u32;
-            let ylhs: u32 = ((lhs.index_geom.y - extent_min_x) / stride_y).trunc() as u32;
-
-            let xrhs: u32 = ((rhs.index_geom.x - extent_min_x) / stride_x).trunc() as u32;
-            let yrhs: u32 = ((rhs.index_geom.y - extent_min_x) / stride_y).trunc() as u32;
-
-            let indexlhs = hilbert_xy_to_index(xlhs, ylhs);
-            let indexrhs = hilbert_xy_to_index(xrhs, yrhs);
-
-            indexlhs.cmp(&indexrhs)
-        });
-    }
-
-    /// Creates a new tree builder with base capacity
-    pub fn new(size: usize) -> Self {
-        HPRTreeBuilder {
-            items: Vec::with_capacity(size),
-            extent: BBox::default(),
-        }
-    }
-
-    /// Builds the index and transfers the builders state into an HPRTree which is then returned, depends on the data being sorted already
-    pub fn build_sorted(mut self) -> HPRTree<T> {
-        if self.items.len() < NODE_CAPACITY {
-            return HPRTree {
-                items: self.items,
-                extent: self.extent,
-                layer_start_index: Vec::new(),
-                node_bounds: Vec::new(),
-            };
-        }
-
-        let layer_start_index = self.compute_layer_start_indices();
-
-        let mut node_bounds = vec![BBox::default(); layer_start_index[layer_start_index.len() - 1]];
-
-        self.compute_leaf_nodes(&layer_start_index, &mut node_bounds);
-        self.compute_layer_nodes(&layer_start_index, &mut node_bounds);
-
-        HPRTree {
-            items: self.items,
-            extent: self.extent,
-            layer_start_index,
-            node_bounds,
-        }
-    }
-
-    /// Sorts the data, builds the index and transfers the builders state into an HPRTree which is then returned
+    /// Sorts the data, builds the index and transfers the builders state into an HPRTree which is then returned. If [sort_items](#method.sort_items) has been called before, prefer [build_sorted](#method.build_sorted) instead
     pub fn build(mut self) -> HPRTree<T> {
         if self.items.len() < NODE_CAPACITY {
             return HPRTree {
@@ -304,6 +259,61 @@ where
         self.sort_items();
 
         self.build_sorted()
+    }
+
+    /// Sorts the contained data (in preparation for [build_sorted](#method.build_sorted))
+    pub fn sort_items(&mut self) {
+        let stride_x = self.extent.width() / H as f32;
+        let stride_y = self.extent.height() / H as f32;
+
+        let extent_min = self.extent.minx.min(self.extent.miny);
+
+        self.items.sort_by_cached_key(|pt| {
+            let x: u32 = ((pt.index_geom.x - extent_min) / stride_x).trunc() as u32;
+            let y: u32 = ((pt.index_geom.y - extent_min) / stride_y).trunc() as u32;
+            hilbert_xy_to_index(x, y)
+        });
+    }
+
+    /// Builds the index and transfers the builders state into an HPRTree which is then returned, depends on the data being sorted (by [sort_items](#method.sort_items)) already
+    pub fn build_sorted(mut self) -> HPRTree<T> {
+        if self.items.len() < NODE_CAPACITY {
+            return HPRTree {
+                items: self.items,
+                extent: self.extent,
+                layer_start_index: Vec::new(),
+                node_bounds: Vec::new(),
+            };
+        }
+
+        let layer_start_index = self.compute_layer_start_indices();
+
+        let mut node_bounds = vec![BBox::default(); *layer_start_index.last().unwrap()];
+
+        self.compute_leaf_nodes(&layer_start_index, &mut node_bounds);
+        self.compute_layer_nodes(&layer_start_index, &mut node_bounds);
+
+        HPRTree {
+            items: self.items,
+            extent: self.extent,
+            layer_start_index,
+            node_bounds,
+        }
+    }
+
+    /// Returns the number of elements in the tree
+    pub fn len(&self) -> usize {
+        self.items.len()
+    }
+
+    /// Returns whether the tree is empty
+    pub fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
+
+    /// Returns the extent of the tree
+    pub fn extent(&self) -> BBox {
+        self.extent.clone()
     }
 
     fn compute_layer_start_indices(&mut self) -> Vec<usize> {
@@ -329,7 +339,7 @@ where
         layer_start_index
     }
 
-    fn compute_leaf_nodes(&mut self, layer_start_index: &Vec<usize>, node_bounds: &mut Vec<BBox>) {
+    fn compute_leaf_nodes(&mut self, layer_start_index: &[usize], node_bounds: &mut [BBox]) {
         for i in 0..layer_start_index[1] {
             for j in 0..=NODE_CAPACITY {
                 let index = NODE_CAPACITY * i + j;
@@ -340,7 +350,7 @@ where
             }
         }
     }
-    fn compute_layer_nodes(&mut self, layer_start_index: &Vec<usize>, node_bounds: &mut Vec<BBox>) {
+    fn compute_layer_nodes(&mut self, layer_start_index: &[usize], node_bounds: &mut [BBox]) {
         for i in 1..(layer_start_index.len() - 1) {
             let layer_start = layer_start_index[i];
             let layer_size = get_layer_size(i, layer_start_index);
@@ -431,13 +441,28 @@ where
         }
     }
 
+    /// Queries the tree by bounding box returning a Vec of the found elements
+    pub fn query(&self, query_env: &BBox) -> Vec<T> {
+        if !self.extent.intersects(query_env) {
+            return Vec::new();
+        }
+
+        let n_guessed_candidates =
+            self.avg_entries() * query_env.height() * query_env.width() * 1.5;
+        let mut candidate_list = Vec::with_capacity((n_guessed_candidates) as usize);
+
+        self.query_with_list(query_env, &mut candidate_list);
+
+        candidate_list
+    }
+
     /// Queries the tree by bounding box and pushes the found elements onto the vector, useful if the usecase enables better estimates for how many elements will be found (to reduce the chance for reallocation or overallocation)
     pub fn query_with_list(&self, query_env: &BBox, candidate_list: &mut Vec<T>) {
         if !self.extent.intersects(query_env) {
             return;
         }
 
-        if self.layer_start_index.len() == 0 {
+        if self.layer_start_index.is_empty() {
             self.query_items(0, query_env, candidate_list);
             return;
         }
@@ -448,21 +473,6 @@ where
         for i in 0..layer_size {
             self.query_node(&layer_index, &i, query_env, candidate_list);
         }
-    }
-
-    /// Queries the tree by bounding box returning a Vec of the found elements
-    pub fn query(&self, query_env: &BBox) -> Vec<T> {
-        if !self.extent.intersects(query_env) {
-            return Vec::new();
-        }
-
-        let mut candidate_list = Vec::with_capacity(
-            (self.avg_entries() * query_env.height() * query_env.width()) as usize,
-        );
-
-        self.query_with_list(query_env, &mut candidate_list);
-
-        candidate_list
     }
 
     /// Returns how many elements are in an area unit on average, may help with guessing how many entities will be found in a given bounding box if the entries are somewhat evenly distributed
@@ -478,8 +488,29 @@ where
             + size_of::<BBox>()
     }
 
+    /// Approximates how many bytes would be taken up by the data of a tree with a given size and index element type
+    pub fn projected_size_in_bytes(elems: usize) -> usize {
+        elems * size_of::<IndexItem<T>>()
+            + (elems as f32).log(NODE_CAPACITY as f32).trunc() as usize * size_of::<usize>()
+            + (elems as f64*0.0667+2.2143).trunc() as usize * size_of::<BBox>()
+            // approximate linear regression from the following values
+            // 16200    64800   145800  259200  405000  583200  793800  1036800
+            // 1082     4323    9722    17283   27002   38882   52921   69124
+            + size_of::<BBox>()
+    }
+
     /// Returns the number of elements in the tree
-    pub fn size(&self) -> usize {
+    pub fn len(&self) -> usize {
         self.items.len()
+    }
+
+    /// Returns whether the tree is empty
+    pub fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
+
+    /// Returns the extent of the tree
+    pub fn extent(&self) -> BBox {
+        self.extent.clone()
     }
 }
